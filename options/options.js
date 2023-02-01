af5ff94bf04a69da4dbf22bea28fa5ecfdcc2254
Crowdbotics <team@crowdbotics.com>
2023-02-01 12:22:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "steep-grass-38630",
+  url: "https://steep-grass-38630.botics.co",
+  api: "https://steep-grass-38630.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
